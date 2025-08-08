446ee9761f1d605e416c6c9d09b1f4369e241dd5
Nilesh Navale <nilesh.navale@jiffyshirts.com>
2025-08-08 10:56:38 +0530

    Updated files for lambda function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.RestAPI);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
