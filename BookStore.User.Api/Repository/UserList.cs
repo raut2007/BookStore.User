08ccea91db32b15e2b424b5770234ceac6b126c6
raut2007 <raut.2007@gmail.com>
2025-08-19 21:37:43 +0530

    New user A1 added
    New user A1 added

--- BookStore.User.Api/Repository/UserList.cs
+++ BookStore.User.Api/Repository/UserList.cs
@@ -8,6 +8,7 @@ namespace BookStore.User.Api.Repository
             { 
                 new UserModel { Id = 101,FirstName = "Vishal", LastName="Raut", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
                 new UserModel { Id = 102,FirstName = "Amol", LastName="More", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
+                new UserModel { Id = 103,FirstName = "ABCD", LastName="EFG", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
             };
     }
 }
