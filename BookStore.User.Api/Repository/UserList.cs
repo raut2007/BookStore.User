fe31e3220dd46e2fb1005c5174028b0bc5f20946
raut2007 <raut.2007@gmail.com>
2025-08-25 20:16:47 +0530

    User updated
    User updated

--- BookStore.User.Api/Repository/UserList.cs
+++ BookStore.User.Api/Repository/UserList.cs
@@ -8,7 +8,7 @@ namespace BookStore.User.Api.Repository
             { 
                 new UserModel { Id = 101,FirstName = "Vishal", LastName="Raut", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
                 new UserModel { Id = 102,FirstName = "Amol", LastName="More", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
-                new UserModel { Id = 103,FirstName = "A1", LastName="A111", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
+                new UserModel { Id = 103,FirstName = "A1", LastName="A11", IsActive = true, IsDeleted=false, CreatedBy = 1, CreatedDate = DateTime.Now, UpdatedBy = 1, UpdatedDate = DateTime.Now },
             };
     }
 }
